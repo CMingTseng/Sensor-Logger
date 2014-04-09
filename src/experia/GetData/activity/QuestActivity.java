3bf9febc2ac4304709ebf00ca813a4564b320e89
Le Van Hoang <hoang8f@gmail.com>
2014-04-09 14:02:11 +0900

    Implement LowPassFiltering

--- src/experia/GetData/activity/QuestActivity.java
+++ src/experia/GetData/activity/QuestActivity.java
@@ -1,25 +1,57 @@
 package experia.GetData.activity;
 
 import android.app.Activity;
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
 import experia.GetData.R;
+import experia.GetData.filter.LowPassFilter;
 
 
-public class QuestActivity extends Activity {
+public class QuestActivity extends Activity implements SensorEventListener {
+
+    public static final String TAG = "QuestActivity";
+
+    // Outputs for the acceleration and LPFs
+    private float[] acceleration = new float[3];
+    private float[] lowPassFilterOutput = new float[3];
+    private LowPassFilter lowPassFilter;
+    private SensorManager sensorManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quest);
+
+        //get sensor manager
+        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+
+        //Init LowPassFilter
+        lowPassFilter = new LowPassFilter();
     }
 
+    @Override
+    protected void onPause() {
+        super.onPause();
+        sensorManager.unregisterListener(this);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        sensorManager.registerListener(this, sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER),SensorManager.SENSOR_DELAY_FASTEST);
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        
+
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.quest, menu);
         return true;
@@ -37,4 +69,16 @@ public class QuestActivity extends Activity {
         return super.onOptionsItemSelected(item);
     }
 
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        System.arraycopy(event.values, 0, acceleration, 0, event.values.length);
+        lowPassFilterOutput = lowPassFilter.addSamples(acceleration);
+        String log = String.format("Raw: %f %f %f Filtered: %f %f %f", acceleration[0], acceleration[1], acceleration[2], lowPassFilterOutput[0], lowPassFilterOutput[1], lowPassFilterOutput[2]);
+        Log.d(TAG, log);
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+    }
 }
